401c7ad22920a5d6720ae5cb0028594babf72433
Matthew Lavallee <54852007+MatthewLavallee@users.noreply.github.com>
2024-12-07 13:33:07 -0500

    add hw_grabber

--- R/folder_setup.R
+++ R/folder_setup.R
@@ -28,3 +28,14 @@ create_project_folder <- function(project_name, base_path = ".") {
   # Return the project path
   return(project_path)
 }
+
+#' Get homeworks.
+#' @description
+#' \code{create_project_folder()} creates a .Rproj object in a new folder given a specified directory. After, it creates a set of dropdown folders.
+grab_homeworks <- function(variables) {
+  num_hws<-1:1
+  hw_urls<-paste0("https://raw.githubusercontent.com/MatthewLavallee/hsr716/refs/heads/main/trunk/homeworks/hw",num_hws,".Rmd")
+  for (i in 1:length(hw_urls)) {
+    download.file(hw_urls[i],here("homeworks",paste0("hw",i,".Rmd")))
+  }
+}
